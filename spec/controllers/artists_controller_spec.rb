49fa9edab22deab66fc7c348614e5fa7c70a8be8
wratterman <will.ratterman@gmail.com>
2017-07-12 20:09:05 -0600

    new and edit tested for artists_controller

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe ArtistsController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET #show" do
+    it "assigns the requested artist as @artist and renders the show template" do
+      artist = create(:artist)
+      get(:show, {:id => artist.to_param})
+      expect(assigns(:artist)).to eq(artist)
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns the builds and empty artist as @artist and renders the new template" do
+      get :new
+      expect(assigns(:artist)).to be_a(Artist)
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested artist as @artist and renders the edit template" do
+      artist = create(:artist)
+      get(:edit, params: {:id => artist.id})
+      expect(assigns(:artist)).to eq(artist)
+      expect(response).to render_template("edit")
+    end
+  end
 end
