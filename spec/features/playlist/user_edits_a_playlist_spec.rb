0d49190e9caa056f4aac69ef5a15311bc683b9d8
wratterman <will.ratterman@gmail.com>
2017-07-12 20:58:44 -0600

    first two login gems

--- spec/features/playlist/user_edits_a_playlist_spec.rb
+++ spec/features/playlist/user_edits_a_playlist_spec.rb
@@ -4,15 +4,15 @@ RSpec.feature "User goes to playlists index" do
   scenario "they see the page with all playlists" do
     existing_playlist = create(:playlist)
     existing_playlist.songs.create(title: "TBD", artist_id: 1)
-    song1 = create(:song)
-    song2 = existing_playlist.songs.last
+    song1 = existing_playlist.songs.first
+    song2 = create(:song)
     name = "Will"
 
     visit playlist_path(existing_playlist)
     click_on "Edit"
     fill_in "playlist_name", with: name
-    check("song-#{song1.id}")
-    uncheck("song-#{song2.id}")
+    uncheck("song-#{song1.id}")
+    check("song-#{song2.id}")
 
     click_on "Update Playlist"
 
