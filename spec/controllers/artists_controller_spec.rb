9e07177e04b3a3905abf78d018f6b96bd22e9742
wratterman <will.ratterman@gmail.com>
2017-07-12 20:02:22 -0600

    added gem 'rails-controller-testing' to get first controller test to pass

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ArtistsController, type: :controller do
+  describe "GET #index" do
+    it "assigns all artists as @artists and renders the index template" do
+      artist = create(:artist)
 
+      get(:index)
+
+      expect(assigns(:artists)).to eq([artist])
+      expect(response).to render_template("index")
+    end
+  end
 end
